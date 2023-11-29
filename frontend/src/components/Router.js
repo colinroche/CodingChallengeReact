7e58a870c21df9ebc2aa25d26c5fe319a3b1190c
colinroche <colinproche@gmail.com>
2023-11-29 14:36:47 +0000

    Adding coutries page and url routing to it

--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -1,6 +1,7 @@
 import Header from './Header'
 import Footer from './Footer'
 import Home from '../pages/Home'
+import Countries from '../pages/Countries'
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 
 export default function Router() {
@@ -18,10 +19,20 @@ export default function Router() {
         {
             path: "/",
             element: <Layout />,
-            children: [{
-                path: "/",
-                element: <Home />
-            }]
+            children: [
+                {
+                    path: "/",
+                    element: <Home />
+                },
+                {
+                    path: 'countries',
+                    element: <Countries />
+                },
+                {
+                    path: 'countries/:id',
+                    element: <Countries />
+                }
+            ]
         }
     ])
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom"
+
 export default function Home() {
     return (
         <div>
             <form className="countryForm">
                 <label>Please enter your desired country below:</label>
                 <input type="text"></input>
-
-                <button type="submit">Submit</button>
+                <Link to="/countries"><button type="submit">Submit</button></Link>
             </form>
         </div>    
     )
