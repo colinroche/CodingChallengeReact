9996f47a6073b2528bc324af14805971b51e4571
colinroche <colinproche@gmail.com>
2023-11-29 13:17:04 +0000

    Basic styling for Home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,12 @@
 export default function Home() {
     return (
         <div>
-            <p>Home</p>
+            <form className="countryForm">
+                <label>Please enter your desired country below:</label>
+                <input type="text"></input>
+
+                <button type="submit">Submit</button>
+            </form>
         </div>    
     )
 }
